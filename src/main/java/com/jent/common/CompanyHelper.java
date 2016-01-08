0c7f55b26b913bc73b1f770efa3de61af15ba0c8
jent <yangjing@xiangrikui.com>
2016-01-08 16:35:32 +0800

    thymeleaf demo

---
+++ src/main/java/com/jent/common/CompanyHelper.java
@@ -0,0 +1,7 @@
+package com.jent.common;
+
+public class CompanyHelper {
+	public  String addTag(String str){
+		return str + " XRK";
+	}
+}
--- src/main/java/com/jent/controller/BaseController.java
+++ src/main/java/com/jent/controller/BaseController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jent.bean.Category;
 import com.jent.bean.Company;
@@ -19,6 +21,7 @@ public class BaseController {
 	
 	@RequestMapping(value ="company/{code}",method = RequestMethod.GET,
 			produces = {"application/json;charset=UTF-8"})
+	@ResponseBody
 	public ModelMap getCompanyByCode(@PathVariable String code){
 		Company c = new CompanyRepository().getCompanyByCode(code);
 		ModelMap resultMap = new ModelMap();
@@ -28,6 +31,7 @@ public class BaseController {
 	
 	@RequestMapping(value ="companiest",method = RequestMethod.GET,
 			produces = {"application/json;charset=UTF-8"})
+	@ResponseBody
 	public ModelMap companyList(){
 		ModelMap resultMap = new ModelMap();
 		resultMap.put("list", new CompanyRepository().getCompanyList());
@@ -36,16 +40,35 @@ public class BaseController {
 	
 	@RequestMapping(value ="/categories",method = RequestMethod.GET,
 			produces = {"application/json;charset=UTF-8"})
-	public ModelMap CategoryList(){
+	@ResponseBody
+	public ModelMap categoryList(){
 		ModelMap resultMap = new ModelMap();
 		resultMap.put("list", new CategoryService().getCategroyList());
+		return resultMap;
+	}
+	
+	@RequestMapping(value ="/company",method = RequestMethod.POST,
+			produces = {"application/json;charset=UTF-8"})
+	@ResponseBody
+	public ModelMap addCompany(
+			@RequestParam(value = "name", required = true) String name,
+			@RequestParam(value = "code",required = true) String code){
+		ModelMap resultMap = new ModelMap();
+		Company c = new Company();
+		c.setCompany_code(code);
+		c.setCompany_name(name);
+		resultMap.put("company", c);
 		return resultMap;
 	}
 	
 	@RequestMapping("/category/view")
 	public String CategoryView(Model model){
 		List<Category> list = new CategoryService().getCategroyList();
+		Company c = new Company();
+		c.setCompany_code("xrk");
+		c.setCompany_name("xiangrikui");
 		model.addAttribute("list", list);
+		model.addAttribute("company", c);
 		return "category";
 	}
 }
